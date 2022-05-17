495558988f2847965a8c8b615b0ab815bac05052
HO <love47024702@naver.com>
2022-05-17 19:24:54 +0900

    그래프 선택 콤보박스패킷 전달

--- src/main/java/com/creative/creativeprojectclient/Func1_1Controller.java
+++ src/main/java/com/creative/creativeprojectclient/Func1_1Controller.java
@@ -1,15 +1,16 @@
 package com.creative.creativeprojectclient;
 
 import body.FluctuationRateWrapper;
+import body.SelectRegionGraphData;
 import com.creative.creativeprojectclient.datamodel.Func1_1TableRowModel;
 import domain.FluctuationRate;
+import domain.Sido;
+import domain.Sigungu;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
 import network.Packet;
@@ -21,8 +22,13 @@ import javafx.scene.layout.AnchorPane;
 import network.protocolCode.RealEstateInfoCode;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+import static javafx.scene.control.Alert.AlertType.WARNING;
 
 public class Func1_1Controller implements Initializable {
 
@@ -32,6 +38,19 @@ public class Func1_1Controller implements Initializable {
     Label result;
     private MainController mainController;
 
+    @FXML
+    ComboBox<String> regionCombo; // 첫번째 그래프 콤보 박스
+    @FXML
+    ComboBox<String> sidoCombo; //두번째 그래프 콤보 박스 시도 선택
+    @FXML
+    ComboBox<String> sigunguCombo;//두번째 그래프 콤보 박스 시군구 선택
+    @FXML
+    Button findFirstGraphBtn; // 첫번째 그래프 찾기 버튼
+    @FXML
+    Button findSecondGraphBtn;// 두번째 그래프 찾기 버튼
+
+    private List<Sido> secondRegionSelectList;//두번째 그래프 지역 정보
+    private List<String> firstRegionSelectList;//첫번째 그래프 지역 정보
     // 아파트
     @FXML
     private TableView<Func1_1TableRowModel> apartment;
@@ -46,42 +65,127 @@ public class Func1_1Controller implements Initializable {
     @FXML
     private TableColumn<Func1_1TableRowModel, String> priceCntColumnApt;
 
-    // 연립다세대
-    @FXML
-    private TableView<Func1_1TableRowModel> rowhouse;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> regionNameColumnRow;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> fluctuationRateColumnRow;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> avgPriceColumnRow;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> populationColumnRow;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> priceCntColumnRow;
-
-    // 단독/다가구
-    @FXML
-    private TableView<Func1_1TableRowModel> detachedHouse;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> regionNameColumnDetach;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> fluctuationRateColumnDetach;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> avgPriceColumnDetach;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> populationColumnDetach;
-    @FXML
-    private TableColumn<Func1_1TableRowModel, String> priceCntColumnDetach;
-
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
+        initSecondRegionSelectCombo(); // 두번째 그래프 지역 콤보 박스 초기화
+        initFirstRegionSelectCombo();
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        firstRegionSelectList = Arrays.asList("전국", "수도권", "지방권", "6대광역시", "5대광역시", "9개도", "8개도");
+    }
 
+    private void initFirstRegionSelectCombo(){// 첫번째 그래프 콤보 박스 초괴화
+        regionCombo.setPromptText("지역");
+
+        ObservableList<String> firstGraphRegion = FXCollections.observableArrayList(firstRegionSelectList);
+        regionCombo.setItems(firstGraphRegion); //  콤보 박스 초기화
+
+        setComboBoxPrompt(regionCombo);
+    }
+
+    private void initSecondRegionSelectCombo(){// 두번째 그래프 시도 콤보 박스 초기화
+        secondRegionSelectList = mainController.getRegionSelectList();// 지역 리스트 초기화
+
+        sidoCombo.setPromptText("시/도");
+        sigunguCombo.setPromptText("시/군/구");
+
+        List<String> sidoNameList = secondRegionSelectList.stream()
+                .map(sido -> sido.getRegionName())
+                .collect(Collectors.toList()); // 시도 이름 리스트 반환
+
+        ObservableList<String> sidos = FXCollections.observableArrayList(sidoNameList);
+        sidoCombo.setItems(sidos); // 시도 콤보 박스 초기화
+
+        setComboBoxPrompt(sidoCombo);
+        setComboBoxPrompt(sigunguCombo);
+    }
+    private void setComboBoxPrompt(ComboBox<String> comboBox) {
+        comboBox.setButtonCell(new ListCell<>() { // Combo box 초기화 시 promptText 강제로 보여주기.
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null || empty) {
+                    setText(comboBox.getPromptText());
+                } else {
+                    setText(item);
+                }
+            }
+        });
+    }
+
+    public void sidoComboChange() {//시도 콤보박스 선택시
+        int selectedSidoIndex = sidoCombo.getSelectionModel().getSelectedIndex();// 선택한 시도 콤보 인덱스
+        List<Sigungu> sigunguList = secondRegionSelectList.get(selectedSidoIndex).getSigunguList();// 선택한 시도에 있는 시군구 리스트 반환
+
+        List<String> sigunguNameList = sigunguList.stream()
+                .map(sigungu -> sigungu.getRegionName())
+                .collect(Collectors.toList());// 선택한 시도에 있는 시군구 이름 리스트 반환
+
+        ObservableList<String> sigungus = FXCollections.observableArrayList(sigunguNameList);
+
+        sigunguCombo.setItems(sigungus); //시도에 맞는 시군구 콤보 박스 설정
+
+    }
+
+    public void findFirstRegion(){
+        int regionComboIndex = regionCombo.getSelectionModel().getSelectedIndex();
+        Packet receivePacket = null;
+
+        if(regionComboIndex<0){
+            alertMessage("지역을 선택해주세요");
+            return;
+        }
+        System.out.println("선택된 지역 이름 : "+firstRegionSelectList.get(regionComboIndex));
+        String regionName = firstRegionSelectList.get(regionComboIndex);
+
+        Packet sendPacket = new Packet(ProtocolType.REAL_ESTATE_INFO.getType(), RealEstateInfoCode.GRAPH_REGION_SELECTION_FIRST_REQ.getCode(),regionName);
+        mainController.writePacket(sendPacket); //패킷 송신
+//        receivePacket = mainController.readPacket(); //패킷 수신
+    }
+
+    public void findSecondRegion(ActionEvent event) { // 두번째 찾기 버튼 클릭시
+        int sidoIndex = sidoCombo.getSelectionModel().getSelectedIndex();
+        int sigunguIndex = sigunguCombo.getSelectionModel().getSelectedIndex();
+        Packet sendPacket = null;
+        Packet receivePacket = null;
+
+        if(sidoIndex<0){
+            alertMessage("시/도를 선택해주세요");
+            return;
+        }
+
+        Sido sido = secondRegionSelectList.get(sidoIndex);
+        SelectRegionGraphData selectRegionGraphData=null;
+
+
+        if(sigunguIndex <0){
+            selectRegionGraphData = new SelectRegionGraphData(sido.getRegionName(), null);
+        }
+        else{
+            Sigungu sigungu = secondRegionSelectList.get(sidoIndex).getSigunguList().get(sigunguIndex);
+            if(sido.getRegionName().equals(sigungu.getRegionName())){ //세종특별자치시 인 경우에는 sido 이름만 넘겨줌.
+                selectRegionGraphData = new SelectRegionGraphData(sido.getRegionName(), null);
+            }
+            else{
+                selectRegionGraphData = new SelectRegionGraphData(sido.getRegionName(), sigungu.getRegionName());
+
+            }
+        }
+        sendPacket = new Packet(ProtocolType.REAL_ESTATE_INFO.getType(), RealEstateInfoCode.GRAPH_REGION_SELECTION_SECOND_REQ.getCode(),selectRegionGraphData);
+
+
+        mainController.writePacket(sendPacket); //패킷 송신
+//        receivePacket = mainController.readPacket(); //패킷 수신
+
+    }
+
+    private void alertMessage(String message){
+        Alert alert = new Alert(WARNING);
+        alert.setHeaderText(message);
+        alert.showAndWait();
     }
 
     @FXML
@@ -99,6 +203,7 @@ public class Func1_1Controller implements Initializable {
             e.printStackTrace();
         }
     }
+
     @FXML
     public void moveToFunc2Controller(){
         try{
@@ -113,6 +218,7 @@ public class Func1_1Controller implements Initializable {
             e.printStackTrace();
         }
     }
+
     @FXML
     public void moveToFunc3_1Controller(){
         try{
@@ -138,9 +244,9 @@ public class Func1_1Controller implements Initializable {
         FluctuationRateWrapper data = (FluctuationRateWrapper)sendPacket.getBody();
 
         showApt(data.getApartmentFluctuationRate());
-        showRowHouse(data.getRowhouseFluctuationRate());
-        showDetached(data.getDetachedhouseFluctuationRate());
+
     }
+
     public void showApt(List<FluctuationRate> data){ //아파트 표 출력
 
         regionNameColumnApt.setCellValueFactory(cellData->cellData.getValue().regionNameProperty());
@@ -196,110 +302,5 @@ public class Func1_1Controller implements Initializable {
         apartment.setItems(myList);
     }
 
-    public void showRowHouse(List<FluctuationRate> data){//연립 다세대
-        regionNameColumnRow.setCellValueFactory(cellData->cellData.getValue().regionNameProperty());
-        fluctuationRateColumnRow.setCellValueFactory(cellData->cellData.getValue().fluctuationRateProperty());
-        avgPriceColumnRow.setCellValueFactory(cellData->cellData.getValue().avgPriceProperty());
-        populationColumnRow.setCellValueFactory(cellData->cellData.getValue().populationProperty());
-        priceCntColumnRow.setCellValueFactory(cellData->cellData.getValue().priceCntProperty());
 
-        ObservableList<Func1_1TableRowModel> myList = FXCollections.observableArrayList();
-
-        for (int i=0;i<data.size();i++){
-            SimpleStringProperty regionName = new SimpleStringProperty(data.get(i).getRegionName());
-            SimpleStringProperty fluctuationRate = new SimpleStringProperty(String.format("%.2f",data.get(i).getFluctuationRateData()));
-            SimpleStringProperty averagePrice = new SimpleStringProperty(String.valueOf(data.get(i).getAveragePrice()));
-            SimpleStringProperty population = new SimpleStringProperty(String.valueOf(data.get(i).getPopulation()));
-            SimpleStringProperty priceCnt = new SimpleStringProperty(data.get(i).getCurrentPriceCnt() + "(" + data.get(i).getLastPriceCnt() + ")");
-            myList.add((new Func1_1TableRowModel(regionName,fluctuationRate,averagePrice,population,priceCnt)));
-        }
-
-
-        fluctuationRateColumnRow.setCellFactory(new Callback<TableColumn<Func1_1TableRowModel, String>, TableCell<Func1_1TableRowModel, String>>() {
-            @Override
-            public TableCell<Func1_1TableRowModel, String> call(TableColumn<Func1_1TableRowModel, String> param) {
-                return new TableCell<Func1_1TableRowModel,String>(){
-                    @Override
-                    protected void updateItem(String item,boolean empty){
-                        if (!empty) {
-                            int currentIndex = indexProperty()
-                                    .getValue() < 0 ? 0
-                                    : indexProperty().getValue();
-                            String fluctuationRate = param
-                                    .getTableView().getItems()
-                                    .get(currentIndex).fluctuationRateProperty().getValue();
-
-                            if (fluctuationRate.substring(0,1).equals("-")) {
-                                setTextFill(Color.BLUE);
-                                setText(fluctuationRate+"%("+data.get(currentIndex).getFluctuationPrice()+")");
-                            }
-                            else{
-                                setTextFill(Color.RED);
-//                                getStyleClass().add("plus-fluctuation");
-                                setText("+"+fluctuationRate+"%("+data.get(currentIndex).getFluctuationPrice()+")");
-
-                            }
-                        }
-                    }
-                };
-            }
-        });
-
-        rowhouse.setItems(myList);
-
-    }
-
-    public void showDetached(List<FluctuationRate> data){// 단독/다가구
-        regionNameColumnDetach.setCellValueFactory(cellData->cellData.getValue().regionNameProperty());
-        fluctuationRateColumnDetach.setCellValueFactory(cellData->cellData.getValue().fluctuationRateProperty());
-        avgPriceColumnDetach.setCellValueFactory(cellData->cellData.getValue().avgPriceProperty());
-        populationColumnDetach.setCellValueFactory(cellData->cellData.getValue().populationProperty());
-        priceCntColumnDetach.setCellValueFactory(cellData->cellData.getValue().priceCntProperty());
-
-        ObservableList<Func1_1TableRowModel> myList = FXCollections.observableArrayList();
-
-        for (int i=0;i<data.size();i++){
-            SimpleStringProperty regionName = new SimpleStringProperty(data.get(i).getRegionName());
-            SimpleStringProperty fluctuationLate = new SimpleStringProperty(String.format("%.2f",data.get(i).getFluctuationRateData()));
-            SimpleStringProperty averagePrice = new SimpleStringProperty(String.valueOf(data.get(i).getAveragePrice()));
-            SimpleStringProperty population = new SimpleStringProperty(String.valueOf(data.get(i).getPopulation()));
-            SimpleStringProperty priceCnt = new SimpleStringProperty(data.get(i).getCurrentPriceCnt() + "(" + data.get(i).getLastPriceCnt() + ")");
-            myList.add((new Func1_1TableRowModel(regionName,fluctuationLate,averagePrice,population,priceCnt)));
-        }
-
-
-        fluctuationRateColumnDetach.setCellFactory(new Callback<TableColumn<Func1_1TableRowModel, String>, TableCell<Func1_1TableRowModel, String>>() {
-            @Override
-            public TableCell<Func1_1TableRowModel, String> call(TableColumn<Func1_1TableRowModel, String> param) {
-                return new TableCell<Func1_1TableRowModel,String>(){
-                    @Override
-                    protected void updateItem(String item,boolean empty){
-                        if (!empty) {
-                            int currentIndex = indexProperty()
-                                    .getValue() < 0 ? 0
-                                    : indexProperty().getValue();
-                            String fluctuationRate = param
-                                    .getTableView().getItems()
-                                    .get(currentIndex).fluctuationRateProperty().getValue();
-
-                            if (fluctuationRate.substring(0,1).equals("-")) {
-                                setTextFill(Color.BLUE);
-                                getStyleClass().add("minus-fluctuation");
-                                setText(fluctuationRate+"%("+data.get(currentIndex).getFluctuationPrice()+")");
-                            }
-                            else{
-                                setTextFill(Color.RED);
-//                                getStyleClass().add("plus-fluctuation");
-                                setText("+"+fluctuationRate+"%("+data.get(currentIndex).getFluctuationPrice()+")");
-
-                            }
-                        }
-                    }
-                };
-            }
-        });
-
-        detachedHouse.setItems(myList);
-
-    }
 }
